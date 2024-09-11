587d2e2c4be94ac8bf3f8d4222a7df370a2e9976
itamar1996 <itay9919@gmail.com>
2024-09-11 15:09:01 +0300

    commit 3

--- script.js
+++ script.js
@@ -9,33 +9,37 @@
 צור פונקציה בשם capitalizeFirstLetter.הפונקציה תקבל מחרוזת בתור פרמטר.הפונקציה תעבור על כל מילה במחרוזת.אם מילה אינה מסתיימת בנקודה, הפונקציה תשנה את האות הראשונה שלה לאות גדולה.החזר את המחרוזת המעודכנת. */
 
 let str2 = "helo world. itamar lavan is hear.";
-let chancestr = (str)=>{
+let chancestr = (str) => {
+ 
+  const replacedString = str.split(" ").map((word)=> {
+  if (word[word.length - 1] != ".") {
     
-}
+    return word[0].toUpperCase() + word.slice(1);
+  }
+  return word; 
+});
+  return replacedString.join(" ");
+};
+chancestr(str2)
+let numsarr2 = [1, 1, 2, 3, 4, 55, 3, 3, 2, 2, 22, , 3, 4, 4, 55, 2];
+let filterdoblenums = (arr) => {
+  let uniqarr = [];
+  arr.forEach((e) => {
+    if (!uniqarr.includes(e)) {
+      uniqarr.push(e);
+    }
+  });
+  console.log(uniqarr);
 
+  return arr;
+};
+filterdoblenums(numsarr2);
 
-let numsarr2 = [1,1,2,3,4,55,3,3,2,2,22,,3,4,4,55,2]
-let filterdoblenums = (arr)=>{
-    let uniqarr =[];
-    arr.forEach( e =>{
-        if(!uniqarr.includes(e)){
-            uniqarr.push(e)
-        }
-    })
-    console.log(uniqarr);
-    
-    return arr;
-    
-}
-filterdoblenums(numsarr2)
-
-
-
-let numsarr = [1,3,5,67,34,3,45,3,4,6,7,8,3,1,45,66,90]
-let filterandsortarr = (arr)=>{
-    arr = arr.filter((e)=>e%2 == 0)
-    arr = arr.sort((a, b) => a - b);
-    return arr    
-}
+let numsarr = [1, 3, 5, 67, 34, 3, 45, 3, 4, 6, 7, 8, 3, 1, 45, 66, 90];
+let filterandsortarr = (arr) => {
+  arr = arr.filter((e) => e % 2 == 0);
+  arr = arr.sort((a, b) => a - b);
+  return arr;
+};
 //filterandsortarr(numsarr)
 //console.log(numsarr);
