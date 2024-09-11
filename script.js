36682ad44ea61d1d89ab2492ac4db179f991eb17
itamar1996 <itay9919@gmail.com>
2024-09-11 14:58:15 +0300

    commit 1 2

---
+++ script.js
@@ -0,0 +1,41 @@
+/*צור פונקציה בשם filterAndSortEvenNumbers.
+הפונקציה תקבל מערך של מספרים בתור פרמטר.
+סנן את המספרים כך שרק המספרים הזוגיים יישארו.
+מיין את המספרים הזוגיים בסדר עולה.
+החזר את המערך המסונן והממוין.
+דרישות לתרגיל 2
+צור פונקציה בשם removeDuplicates.הפונקציה תקבל מערך של מספרים בתור פרמטר.הפונקציה תסיר את כל המספרים הכפולים ותשמור רק מספרים ייחודיים.החזר מערך חדש שבו כל מספר מופיע רק פעם אחת, בסדר שבו הוא הופיע לראשונה.
+דרישות תרגיל 3
+צור פונקציה בשם capitalizeFirstLetter.הפונקציה תקבל מחרוזת בתור פרמטר.הפונקציה תעבור על כל מילה במחרוזת.אם מילה אינה מסתיימת בנקודה, הפונקציה תשנה את האות הראשונה שלה לאות גדולה.החזר את המחרוזת המעודכנת. */
+
+let str2 = "helo world. itamar lavan is hear.";
+let chancestr = (str)=>{
+    
+}
+
+
+let numsarr2 = [1,1,2,3,4,55,3,3,2,2,22,,3,4,4,55,2]
+let filterdoblenums = (arr)=>{
+    let uniqarr =[];
+    arr.forEach( e =>{
+        if(!uniqarr.includes(e)){
+            uniqarr.push(e)
+        }
+    })
+    console.log(uniqarr);
+    
+    return arr;
+    
+}
+filterdoblenums(numsarr2)
+
+
+
+let numsarr = [1,3,5,67,34,3,45,3,4,6,7,8,3,1,45,66,90]
+let filterandsortarr = (arr)=>{
+    arr = arr.filter((e)=>e%2 == 0)
+    arr = arr.sort((a, b) => a - b);
+    return arr    
+}
+//filterandsortarr(numsarr)
+//console.log(numsarr);
